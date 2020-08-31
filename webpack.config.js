2e92797a386c9dc2c412c47cd906f2510482b651
Visual Demand <jaybalderas@visualdemand.co>
2020-08-31 12:50:10 -0700

    modified to add the node modules to uglify

--- webpack.config.js
+++ webpack.config.js
@@ -106,7 +106,7 @@ module.exports = {
       },
       {
         test: /\.js$/,
-        exclude: /node_modules/,
+        // exclude: /node_modules/,
         loader: 'babel-loader'
       },
       {
